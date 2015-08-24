14cacc963e9a0f108f88b03f5751da958e6b6f27
Nathen Harvey <nharvey@chef.io>
2015-08-24 16:49:56 -0400

    initial chef-server.rb for the install

---
+++ chef-server.rb
@@ -0,0 +1,8 @@
+server_name = "ec2-54-237-123-168.compute-1.amazonaws.com"
+api_fqdn server_name
+bookshelf['vip'] = server_name
+nginx['url'] = "https://#{server_name}"
+nginx['server_name'] = server_name
+nginx['ssl_certificate'] = "/var/opt/opscode/nginx/ca/#{server_name}.crt"
+nginx['ssl_certificate_key'] = "/var/opt/opscode/nginx/ca/#{server_name}.key"
+lb['fqdn'] = server_name
