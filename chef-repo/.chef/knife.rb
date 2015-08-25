5d80b087a19c000d25b2744b41f5a86da8679c83
Nathen Harvey <nharvey@chef.io>
2015-08-24 20:25:31 -0400

    chef repo with necessary config for knife

---
+++ chef-repo/.chef/knife.rb
@@ -0,0 +1,11 @@
+# See https://docs.getchef.com/config_rb_knife.html for more information on knife configuration options
+
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "chefadmin"
+client_key               "#{current_dir}/chefadmin.pem"
+validation_client_name   "chef-validator"
+validation_key           "#{current_dir}/chef-validator.pem"
+chef_server_url          "https://ec2-54-237-123-168.compute-1.amazonaws.com/organizations/chef"
+cookbook_path            ["#{current_dir}/../cookbooks"]
