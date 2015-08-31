e139284d011962b9abc9a0b385e5da4f21b8b862
Nathen Harvey <nharvey@chef.io>
2015-08-30 22:05:38 -0400

    bump version

--- chef-repo/cookbooks/my_chef-client/metadata.rb
+++ chef-repo/cookbooks/my_chef-client/metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures my_chef-client'
 long_description 'Installs/Configures my_chef-client'
-version '0.1.0'
+version '0.2.0'
 
 depends 'chef-client'
