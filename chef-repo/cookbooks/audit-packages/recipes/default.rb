8a3993ade9c656195f2ad76c8b2bb6058cef548f
Nathen Harvey <nharvey@chef.io>
2015-08-30 22:45:54 -0400

    add support for ubuntu

--- chef-repo/cookbooks/audit-packages/recipes/default.rb
+++ chef-repo/cookbooks/audit-packages/recipes/default.rb
@@ -3,12 +3,25 @@
 # Recipe:: default
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
-packages = %w(openssl ca-certificates openssh tzdata)
-control_group 'Critical Packages' do
-  packages.each do |package|
-    control package do
-      it 'is up to date' do
-        expect(command("yum list updates #{package} | grep '^#{package}\.'").exit_status).to_not eq 0
+if node['platform_family'] == 'rhel'
+  packages = %w(openssl ca-certificates openssh tzdata)
+  control_group 'Critical Packages' do
+    packages.each do |package|
+      control package do
+        it 'is up to date' do
+          expect(command("yum list updates #{package} | grep '^#{package}\.'").exit_status).to_not eq 0
+        end
+      end
+    end
+  end
+elsif node['platform_family'] == 'debian'
+  packages = %w(openssl ca-certificates openssh-client openssh-server tzdata)
+  control_group 'Critical Packages' do
+    packages.each do |package|
+      control package do
+        it 'is up to date' do
+          expect(command("dpkg --get-selections | xargs apt-cache policy {} | grep -1 Installed | sed -r 's/(:|Installed: |Candidate: )//' | uniq -u | tac | sed '/--/I,+1 d' | tac | sed '$d' | sed -n 1~2p | grep '^#{package}$'").exit_status).to_not eq 0
+        end
       end
     end
   end
