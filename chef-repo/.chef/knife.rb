12b66b051ddcd32bc32c36e2572ad330dff94a29
Nathen Harvey <nharvey@chef.io>
2015-08-31 16:12:09 -0400

    Add analytics url to knife

--- chef-repo/.chef/knife.rb
+++ chef-repo/.chef/knife.rb
@@ -10,6 +10,9 @@ validation_key           "#{current_dir}/chef-validator.pem"
 chef_server_url          "https://ec2-54-237-123-168.compute-1.amazonaws.com/organizations/chef"
 cookbook_path            ["#{current_dir}/../cookbooks"]
 
+analytics_server_url "https://ec2-54-237-34-151.compute-1.amazonaws.com/organizations/chef"
+
+
 knife[:aws_ssh_key_id]  = ENV['NATHENHARVEY_AWS_KEYPAIR_NAME']
 knife[:aws_access_key_id]  = ENV['NATHENHARVEY_AWS_ACCESS_KEY_ID']
 knife[:aws_secret_access_key] = ENV['NATHENHARVEY_AWS_SECRET_ACCESS_KEY']
